81278a7a18bbc7e07a8559f393e046c0fbe226dd
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-19 20:12:02 +0600

    Final update Done

--- code/code.ino
+++ code/code.ino
@@ -1,6 +1,6 @@
+// Including library for servo, keypad etc.
 #include <Servo.h>
 #include <LiquidCrystal.h>
-
 #include <Keypad.h>
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
@@ -28,71 +28,67 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 
-
+// LED pin number that are connected with arduino
+// each LED is connected with LDR which indicates which LDR is provinding Maximum value
 int led_1 = 22;
 int led_2 = 23;
 int led_3 = 24;
 
+// LDR pin number (that is connected with arduino)
 int ldr_1 = A4;
 int ldr_2 = A5;
 int ldr_3 = A6;
 
+// charge and discharge pin number 
+// [from solar inverter to the arduino]
 int charge = 52;
 int discharge = 53;
 
-bool check_1 = false;
-bool check_2 = false;
-bool check_3 = false;
+int current_pos = 0; // current position of the servo
+int next_pos = 0; // next position of the servo
 
-bool cp = false;
-bool dp = false;
-
-int current_pos = 0;
-int next_pos = 0;
-
+// value getting from LDR
 int value_1 = 0;
 int value_2 = 0;
 int value_3 = 0;
-int value_4 = 0;
-int value_5 = 5;
 
+// 
 int previous_pos = 0;
 
+// solar position fixed for manual mode
 int solar_pos_1 = 57;
 int solar_pos_2 = 92;
 int solar_pos_3 = 128;
 
+// distinguishing auto and manual mode by using boolean value
+bool isInAuto = true; // as in the beginning the system is in the auto mode, so default true
+bool isInManual = false; // when user press button '3', the system jumps to manual mode, variable becomes true
 
-bool isInAuto = true;
-bool isInManual = false;
-
+// max value among the LDR
 int maxx = 0;
 
 
-
-
 /// prototype
+void solarMode(); // Runs both auto and manual mode
+void turnOnLed(); // Turns the LED according to the maximum LDR value
+void controlPad(); // All keypad related code [for manual mode]
+void printSolarStatus(); // Is System in auto or manual mode
+void displayChargingStatus(); // Are capacitor and the battery charging or not
+void temperature(); // Calculate the temparature provided by the LM35 and show it to LCD
+void solarManual(); // Manual Mode of the system
+void solarAuto(); // Auto mode of the system
+void rotateRight(int current_pos, int next_pos); // Servo rotate clockwise from starting to ending position
+void rotateLeft(int current_pos, int next_pos); // Servo rotate anti-clockwise from starting to ending position
 
-void solarMode();
-void turnOnLed();
-void controlPad();
-void printSolarStatus();
-void displayChargingStatus();
-void temperature();
-void solarManual();
-void solarAuto();
-void rotateRight(int current_pos, int next_pos);
-void rotateLeft(int current_pos, int next_pos);
-void turnOnLed();
 
 
 void setup() {
-
-  lcd.begin(16, 2);
+  
+  lcd.begin(16, 2); // initializing lcd display
   Serial1.begin(9600);
-  myservo.attach(3);
+  myservo.attach(3); // servo attached with arduino's pin 3
 
-  pinMode(led_1, OUTPUT);
+  pinMode(led_1, OUTPUT); 
   pinMode(led_2, OUTPUT);
   pinMode(led_3, OUTPUT);
 
@@ -102,20 +98,15 @@ void setup() {
 
 
 void loop() {
-  
   controlPad();
   solarMode();
-  
 }
 
 void solarMode(){
   if(isInAuto == true){
     solarAuto();
   }
-  else if(isInManual == true){
-//    solarManual();
-  }
-  turnOnLed();
+  turnOnLed(); // qs
 }
 
 
@@ -124,7 +115,7 @@ void controlPad(){
 
   if(c == '1'){
     displayChargingStatus();
-    printSolarStatus();
+    printSolarStatus(); // Manual / Auto Mode
     delay(2000);
     lcd.clear();
   } 
@@ -151,29 +142,22 @@ void controlPad(){
   else if(isInManual && c == '7'){
     lcd.setCursor(0,1);
     lcd.print("Pos: 1");
-    float a = myservo.read();
     myservo.write(solar_pos_1);
   }
   else if(isInManual && c == '8'){
     lcd.setCursor(0,1);
     lcd.print("Pos: 2");
-    float a = myservo.read();
     myservo.write(solar_pos_2);
   }
   else if(isInManual && c == '9'){
     lcd.setCursor(0,1);
     lcd.print("Pos: 3");
-    float a = myservo.read();
-//    lcd.print(a);
     myservo.write(solar_pos_3);
   }
-
-
    printSolarStatus();
    c = '0';
    lcd.setCursor(0,0);
    lcd.print("Select Option");
-
 }
 
 void printSolarStatus(){
@@ -198,7 +182,6 @@ void displayChargingStatus(){
   }
 }
 
-
 void temperature() {
   lcd.clear();
   lcd.setCursor(0,0);
@@ -211,91 +194,60 @@ void temperature() {
   lcd.print(temp_val);
 }
 
-
-
 void solarAuto() {
+  // getting value from the LDR
   value_1 = analogRead(ldr_1);
   value_2 = analogRead(ldr_2);
   value_3 = analogRead(ldr_3);
-
-
+  // check which LDR provides the max value according 
+  // to the light intensity of the torch
   if (value_1 > value_2 && value_1 > value_3) {
     maxx = value_1;
-//    Serial1.println("LDR 1 is maximum");
-    if (check_1 == false) {
-      current_pos = previous_pos;
-      next_pos = solar_pos_1;
-      if (next_pos > previous_pos) {
-        rotateRight(current_pos, next_pos);
-
-      }
-      else {
-        rotateLeft(current_pos, next_pos);
-      }
-
-      check_1 = true;
-      check_2 = false;
-      check_3 = false;
-      previous_pos = next_pos;
+    current_pos = previous_pos; // initially prev pos is '0'
+    next_pos = solar_pos_1;
+    if (next_pos > previous_pos) {
+      rotateRight(current_pos, next_pos);
     }
-
+    else {
+      rotateLeft(current_pos, next_pos);
+    }
   }
   else if (value_2 > value_1 && value_2 > value_3) {
     maxx = value_2;
-//    Serial1.println("LDR 2 is Maximum");
-    if (check_2 == false) {
-      current_pos = previous_pos;
-      next_pos = solar_pos_2;
-      if (next_pos > previous_pos) {
-        rotateRight(current_pos, next_pos);
-      }
-      else {
-        rotateLeft(current_pos, next_pos);
-      }
-      check_2 = true;
-      check_1 = false;
-      check_3 = false;
-      previous_pos = next_pos;
+    current_pos = previous_pos;
+    next_pos = solar_pos_2;
+    if (next_pos > previous_pos) {
+      rotateRight(current_pos, next_pos);
     }
+    else {
+      rotateLeft(current_pos, next_pos);
+    }
+    previous_pos = next_pos;
   }
-
+  
   else if (value_3 >= value_1 && value_3 >= value_2) {
     maxx = value_3;
-//    Serial1.println("LDR 3 is Maximum");
-    if (check_3 == false) {
-      current_pos = previous_pos;
-      next_pos = solar_pos_3;
-      if (next_pos > previous_pos) {
-        rotateRight(current_pos, next_pos);
-      }
-      else {
-        rotateLeft(current_pos, next_pos);
-      }
-      check_3 = true;
-      check_1 = false;
-      check_2 = false;
-      previous_pos = next_pos;
+    current_pos = previous_pos;
+    next_pos = solar_pos_3;
+    if (next_pos > previous_pos) {
+      rotateRight(current_pos, next_pos);
     }
+    else {
+      rotateLeft(current_pos, next_pos);
+    }
+    previous_pos = next_pos;
   }
-
 }
-
+// rotating clockwise
 void rotateRight(int current_pos, int next_pos) {
-//  Serial1.println("Servo is rotating RIGHT");
-//  Serial1.print("current_pos: ");
-//  Serial1.println(current_pos);
-  for (pos = current_pos; pos <= next_pos; pos++) { // goes from 0 degrees to 360 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    //    delay(20);                       // waits 20ms for the servo to reach the position
+  for (pos = current_pos; pos <= next_pos; pos++) {
+    myservo.write(pos);
   }
 }
+// rotating anti-clockwise
 void rotateLeft(int current_pos, int next_pos) {
-//  Serial1.println("servo is rotating LEFT");
-//  Serial1.print("current_pos: ");
-//  Serial1.println(current_pos);
-  for (pos = current_pos; pos > next_pos; pos--) { // goes from 360 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    //    delay(20);                       // waits 20ms for the servo to reach the position
+  for (pos = current_pos; pos > next_pos; pos--) {
+    myservo.write(pos);
   }
 }
 
